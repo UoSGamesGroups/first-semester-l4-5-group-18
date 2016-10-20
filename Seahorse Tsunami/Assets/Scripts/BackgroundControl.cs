79284660b4fba3bd9892339d99ab6b543f5bc7bd
DannyJamieson <danj1066@hotmail.co.uk>
2016-10-20 14:38:59 +0100

    initial commit of game

---
+++ Seahorse Tsunami/Assets/Scripts/BackgroundControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundControl : MonoBehaviour {
+	public SeaHorseControl TargetX;
+	public Vector3 bob; 
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	
+	}
+	
+	void OnMouseDown(){
+		Debug.Log("Mouse X = " + Input.mousePosition.x);
+		Debug.Log("Mouse Y = " + Input.mousePosition.y);
+		//TargetX.TargetX=Input.mousePosition.x;
+		bob = camera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camera.nearClipPlane));
+		TargetX.TargetX=bob.x;
+	}
+}
---
+++ Seahorse Tsunami/Assets/Scripts/SeaHorseControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeaHorseControl : MonoBehaviour {
+	public float TargetX;
+	public float Speed;
+	public float ScaleSize;
+
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		ScaleSize =  transform.localScale.x;
+		TargetX = transform.position.x;
+		//Velocity = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((TargetX >= transform.position.x-0.1)	&& (TargetX<transform.position.x+0.1)){
+		//	Velocity.velocity.x=0;
+		transform.Translate(new Vector3(0,0,0) * Time.deltaTime, Space.World);
+		}
+		
+		else if(TargetX < transform.position.x){
+			//Velocity.velocity.x = -Speed;
+			transform.Translate(new Vector3(-Speed,0,0) * Time.deltaTime, Space.World);
+			 transform.localScale =  new Vector3(ScaleSize,transform.localScale.y,transform.localScale.z);
+		}
+		else if(TargetX>transform.position.x){
+			//Velocity.velocity.x = Speed;
+			transform.Translate(new Vector3(Speed,0,0) * Time.deltaTime, Space.World);
+			transform.localScale =  new Vector3(-ScaleSize,transform.localScale.y,transform.localScale.z);
+		}
+		
+		
+		
+	
+	}
+}
